288482f9d1759e97f9702daa042eabbb13e7a8c6
Soju <gattadesmond@gmail.com>
2025-09-26 12:53:25 +0700

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,22 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd'],
+          refine: ['@refinedev/core', '@refinedev/antd', '@refinedev/react-router']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 });
